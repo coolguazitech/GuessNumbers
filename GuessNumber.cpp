b14185a62c1793c9de49d8f559e3172cd3b67093
Hans <coolguazi@gmail.com>
2021-05-22 05:22:16 +0800

    begin new experiment

--- GuessNumber.cpp
+++ GuessNumber.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <random>
 #include <set>
+#include <string>
 
 Game_host::Game_host()
 {
@@ -51,17 +52,127 @@ bool Player::set_guess(const vector<short> & guess)
     if(validate_digits(guess))
     {
         m_guess = guess;
-        set_is_first_guess(true);
+        // set_is_first_guess(true);
         return true;
     }
     return false;
 }
 
+Solver::Solver(short level) : m_level(level)
+{
+    m_rest_of_choices = get_all_permutations(level);
+}
+
+Solver::Solver()
+{
+    m_rest_of_choices = get_all_permutations(4);
+}
+
+void Solver::take_guess(void)
+{
+    m_guess = m_rest_of_choices[0];
+}
+
+void Solver::update(string result)
+{
+    vector<short> & target = m_guess;
+    vector<vector<short>> & choices = m_rest_of_choices;
+    short t_A = stoi(result.substr(0, 1), nullptr), t_B = stoi(result.substr(4, 1), nullptr), size = target.size(), A = 0, B = 0;
+
+    auto it = choices.begin();
+    while(it != choices.end())
+    {
+        A = 0, B = 0;
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                if(target[i] == it->at(j))
+                {
+                    if(i == j)
+                    {
+                        A++;
+                        break;
+                    }
+                    else
+                    {
+                        B++;
+                        break;
+                    }
+                }
+            }
+        }
+        if(A != t_A || B != t_B || *it == target)
+        {
+            it = choices.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
+}
+
+const vector<vector<short>> Solver::get_all_permutations(short level)
+{
+    vector<short> all_digits {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, rest_of_digits, temp_permutation;
+    vector<vector<short>> all_permutations;
+    if(level == 3)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                for(int k = 0; k < 8; k++)
+                {
+                    rest_of_digits = all_digits;
+                    temp_permutation.push_back(rest_of_digits[i]);
+                    rest_of_digits.erase(rest_of_digits.begin() + i);
+                    temp_permutation.push_back(rest_of_digits[j]);
+                    rest_of_digits.erase(rest_of_digits.begin() + j);
+                    temp_permutation.push_back(rest_of_digits[k]);
+                    all_permutations.push_back(temp_permutation);
+                    temp_permutation.clear();
+                }
+            }
+        }
+    }
+    else
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                for(int k = 0; k < 8; k++)
+                {
+                    for(int l = 0; l < 7; l++)
+                    {
+                        rest_of_digits = all_digits;
+                        temp_permutation.push_back(rest_of_digits[i]);
+                        rest_of_digits.erase(rest_of_digits.begin() + i);
+                        temp_permutation.push_back(rest_of_digits[j]);
+                        rest_of_digits.erase(rest_of_digits.begin() + j);
+                        temp_permutation.push_back(rest_of_digits[k]);
+                        rest_of_digits.erase(rest_of_digits.begin() + k);
+                        temp_permutation.push_back(rest_of_digits[l]);
+                        all_permutations.push_back(temp_permutation);
+                        temp_permutation.clear(); 
+                    }
+
+                }
+            }
+        }
+    }
+    return all_permutations;
+}
+
+
 string operator==(const Game_host & host, const Player & player)
 {
     auto target = host.get_target();
     auto guess = player.get_guess();
-    int A = 0, B = 0, size = target.size();
+    short A = 0, B = 0, size = target.size();
+
     for(int i = 0; i < size; i++)
     {
         for(int j = 0; j < size; j++)
@@ -90,7 +201,66 @@ string operator==(const Player & player, const Game_host & host)
 {
     auto target = host.get_target();
     auto guess = player.get_guess();
-    int A = 0, B = 0, size = target.size();
+    short A = 0, B = 0, size = target.size();
+
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(guess[i] == target[j])
+            {
+                if(i == j)
+                {
+                    A++;
+                    break;
+                }
+                else
+                {
+                    B++;
+                    break;
+                }
+            }
+        }
+    }
+    string result = to_string(A) + " A " + to_string(B) + " B";
+    return result;
+}
+
+string operator==(const Game_host & host, const Solver & solver)
+{
+    auto target = host.get_target();
+    auto guess = solver.get_guess();
+    short A = 0, B = 0, size = target.size();
+
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(guess[i] == target[j])
+            {
+                if(i == j)
+                {
+                    A++;
+                    break;
+                }
+                else
+                {
+                    B++;
+                    break;
+                }
+            }
+        }
+    }
+    string result = to_string(A) + " A " + to_string(B) + " B";
+    return result;
+
+}
+
+string operator==(const Solver & solver, const Game_host & host)
+{
+    auto target = host.get_target();
+    auto guess = solver.get_guess();
+    short A = 0, B = 0, size = target.size();
     
     for(int i = 0; i < size; i++)
     {
--- GuessNumber.h
+++ GuessNumber.h
@@ -24,14 +24,32 @@ class Player
 {
     private:
         vector<short> m_guess; // the taken guess
-        bool is_first_guess = false; // check if first guess is taken
+        // bool is_first_guess = false; // check if first guess is taken
         bool validate_digits(const vector<short>&); // validate if digits are legal
 
     public:
         bool set_guess(const vector<short> &); // set guess, return true if succeed
         const vector<short> & get_guess(void) const {return m_guess;} // get guess
-        bool get_is_first_guess(void) const {return is_first_guess;} // get is_first_guess
-        void set_is_first_guess(bool flag){is_first_guess = flag;} // set is_first_guess
+        // bool get_is_first_guess(void) const {return is_first_guess;} // get is_first_guess
+        // void set_is_first_guess(bool flag){is_first_guess = flag;} // set is_first_guess
         friend string operator==(const Player &, const Game_host &); // compare Player's guess to Game_host's target 
         friend string operator==(const Game_host &, const Player &); // compare Game_host's target to Player's guess
+};
+
+class Solver
+{
+    private:
+        vector<short> m_guess;
+        vector<vector<short>> m_rest_of_choices;
+        short m_level;
+        const vector<vector<short>> get_all_permutations(short);
+
+    public:
+        Solver();
+        Solver(short);
+        void take_guess(void); // take a guess
+        const vector<short> & get_guess(void) const {return m_guess;} // get guess
+        void update(string);
+        friend string operator==(const Solver &, const Game_host &);
+        friend string operator==(const Game_host &, const Solver &);
 };
